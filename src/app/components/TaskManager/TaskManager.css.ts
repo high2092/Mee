cf1cd37ac2d96178a272718cd17f1eebbfb8891e
hhyuw <high2092@gmail.com>
2023-12-19 22:04:45 +0900

    design: 폰트 적용

--- src/app/components/TaskManager/TaskManager.css.ts
+++ src/app/components/TaskManager/TaskManager.css.ts
@@ -3,11 +3,15 @@ import { style } from '@vanilla-extract/css';
 export const taskManagerStyle = style({
   position: 'relative',
   height: '90vh',
+  padding: '0.5rem',
+
   display: 'flex',
 });
 
 export const timeStampStyle = style({
   height: '100%',
+  paddingRight: '0.5rem',
+
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
